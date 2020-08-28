d03fe7ff3b30364aef3219baeb2e1c2f4e6c8567
x1b6e6 <ftdabcde@gmail.com>
2020-08-28 10:53:23 +0700

    test: add new tests for compiling all functions in Net and SimpleNet
    simple_functions: testing functions in SimpleNet
    net_functions: testing function in Net
    
    Signed-off-by: x1b6e6 <ftdabcde@gmail.com>

---
+++ test/net_functions.cc
@@ -0,0 +1,28 @@
+#include <Net.hh>
+
+using net_type = net::Net<2, 2>;
+
+int main() {
+	try {
+		// check throwing exception at invalid argument
+		net_type n(1);
+		return 1;
+	} catch (std::invalid_argument& e) {
+	}
+
+	net_type n(5);
+
+	net_type::feed_type data;
+
+	n.feed(data);
+	n.count_score([](const net_type::result_type&) { return 0; });
+	n.next<std::less>(5);
+
+	auto a = n.rand();
+	auto b = n.reset_score();
+	auto c = n.best_score();
+	auto d = n.result();
+	auto e = n.best_result();
+
+	return 0;
+}
---
+++ test/simple_functions.cc
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <sstream>
+
+#include <Net.hh>
+
+int main() {
+	net::SimpleNet<2, 3, 5> n;
+
+	n++;
+	++n;
+	n + 5;
+	n + n;
+	n.rand();
+	n.merge(n);
+	n.mutation(2);
+
+	std::stringstream ss;
+
+	ss << n;
+
+	net::SimpleNet<2, 3, 5> n2;
+	ss >> n2;
+
+	assert(n == n2);
+
+	return 0;
+}
