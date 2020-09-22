2ba8332ff46730834a878ea6c353125820a064cb
x1b6e6 <ftdabcde@gmail.com>
2020-09-22 21:52:44 +0700

    add email address to license
    Signed-off-by: x1b6e6 <ftdabcde@gmail.com>

--- Net.hh
+++ Net.hh
@@ -1,6 +1,6 @@
 /* MIT License
  *
- * Copyright (c) 2020 x1b6e6
+ * Copyright (c) 2020 x1b6e6 <ftdabcde@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
